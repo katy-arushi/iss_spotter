3d66d05e361f85b26ae4b5ea8413e601d7078f62
Arushi Katyal <katy.arushi@gmail.com>
2021-08-27 17:05:30 -0400

    implement iss spotter using promises

--- index2.js
+++ index2.js
@@ -1,4 +1,4 @@
-const { nextISSTimesForMyLocation } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss_promised')
 
 const printPassTimes = function(passTimes) {
   for (const pass of passTimes) {
@@ -9,9 +9,11 @@ const printPassTimes = function(passTimes) {
   }
 }
 
-nextISSTimesForMyLocation((error, passTimes) => {
-  if (error) {
-    return console.log("It didn't work!", error);
-  }
-  printPassTimes(passTimes);
-});
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
+
