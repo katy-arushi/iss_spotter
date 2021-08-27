98619018ec021bb2b5d2320edb34127850513b42
Arushi Katyal <katy.arushi@gmail.com>
2021-08-27 01:11:04 -0400

    remove unnescessary comment in index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const { nextISSTimesForMyLocation } = require('./iss');
 
 const printPassTimes = function(passTimes) {
   for (const pass of passTimes) {
-    //  for (const pass of Object.values(passTimes.response)) {
     const datetime = new Date(0);
     datetime.setUTCSeconds(pass.risetime);
     const duration = pass.duration;
